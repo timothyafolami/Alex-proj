3b8425fd5765e1ac35f4a103aecd0439e5f60abe
Afolami Timothy Olawale <timmyafolami8469@gmail.com>
2024-03-17 04:07:27 +0100

    cleaned

--- main.py
+++ main.py
@@ -163,10 +163,6 @@ with response_container:
                 message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
 
 
-print(st.session_state['requests'])
-print(st.session_state['responses'])
-print(get_conversation_string())
-
 # checking for order placed !!! in the responses
 if "ORDER PLACED!!!" in st.session_state['responses'][-1]:
     st.balloons()
